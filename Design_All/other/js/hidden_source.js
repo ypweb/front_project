c3a85f0b41e95d3e2b224656322025c4bff51956
yipin8715@sina.com <yipin8715@sina.com>
2018-04-03 17:11:12 +0800

    front_project

--- Design_All/other/js/hidden_source.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-$(function(){
-	//var cur_source=document.body.outerHTML;
-	//alert(cur_source);
-	//document.open();
-	//document.close();
-	//document.title="";
-	//document.body.innerHTML=cur_source;
-});
